2ca11ca95d09580c587ec7b8353df8a744ba7e35
toushobun <92347214+toushobun@users.noreply.github.com>
2026-06-03 23:00:20 +0900

    refactor: 使用账户页面别名引用新增表单依赖

--- src/components/accounts/AccountForm.tsx
+++ src/components/accounts/AccountForm.tsx
@@ -5,8 +5,8 @@ import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 
-import { createAccount } from "accounts/actions";
-import { accountTypeOptions } from "accounts/types";
+import { createAccount } from "account-page/actions";
+import { accountTypeOptions } from "account-page/types";
 
 type AccountFormProps = {
   defaultCurrency: string;
