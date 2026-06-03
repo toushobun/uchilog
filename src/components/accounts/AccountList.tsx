175e65acc6b36673d3d2da6749cd9c7d3d8502f0
toushobun <92347214+toushobun@users.noreply.github.com>
2026-06-03 18:24:11 +0900

    refactor: 移动账户列表组件

---
+++ src/components/accounts/AccountList.tsx
@@ -0,0 +1,46 @@
+import Stack from "@mui/material/Stack";
+
+import { archiveAccount } from "@/app/(protected)/accounts/actions";
+import { type AccountRow } from "@/app/(protected)/accounts/types";
+import { EmptyState } from "@/components/ui/EmptyState";
+
+import { AccountCard } from "./AccountCard";
+import { AccountEditForm } from "./AccountEditForm";
+import { ArchiveAccountButton } from "./ArchiveAccountButton";
+
+type AccountListProps = {
+  accounts: AccountRow[];
+};
+
+export function AccountList({ accounts }: AccountListProps) {
+  if (accounts.length === 0) {
+    return <EmptyState title="还没有账户" description="请先新增一个账户。" />;
+  }
+
+  return (
+    <Stack spacing={2.5} sx={{ mt: 4 }}>
+      {accounts.map((account) => (
+        <AccountCard
+          key={account.id}
+          name={account.name}
+          type={account.type}
+          currency={account.currency}
+          holders={account.holders}
+          initialBalance={account.initial_balance}
+          currentBalance={account.current_balance}
+          actions={
+            <Stack
+              component="form"
+              action={archiveAccount}
+              sx={{ alignSelf: { xs: "stretch", sm: "flex-start" } }}
+            >
+              <input name="accountId" type="hidden" value={account.id} />
+              <ArchiveAccountButton />
+            </Stack>
+          }
+          footer={<AccountEditForm account={account} />}
+        />
+      ))}
+    </Stack>
+  );
+}
